9fae9d02578a29eef0ab78c6edd534f945be22fe
SzymonPT <polytopia72@gmail.com>
2026-03-06 12:06:47 +0100

    front end

--- app/src/main/java/com/onaar/databasesvol2/MainActivity.java
+++ app/src/main/java/com/onaar/databasesvol2/MainActivity.java
@@ -18,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         konflliktyDataBase = KonflliktyDataBase.zwrocInstancjeBazyDanych(MainActivity.this);
-        konflliktyDataBase.zwrocKonfliktyDao().wstawKonfliktDoBazy(new Baza("I Wojna Czeczeńska",1994,1996, "Rosja i Republika Czeczeńska", "Zwycięstwo Czeczeni",  "Zabici:80 000 - 160 000, Ranni: 120 000+, Zaginieni: 5000"));
+        konflliktyDataBase.zwrocKonfliktyDao().wstawKonfliktDoBazy(new Baza("I Wojna Czeczeńska",1994,1996, "Rosja i Republika Czeczeńska", "Zwycięstwo Czeczeni",  "Zabici: 80 000 - 160 000, Ranni: 120 000+, Zaginieni: 5000"));
     }
 }
